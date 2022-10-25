5d6a48aff23b659306db2fd1fcf9b464333bb817
sifa43 <sifatunnoor@gmail.com>
2022-10-25 18:37:31 +0600

    second update

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const Port = process.env.Port || 5000;
+app.use(cors());
+
+const category = require("./Data/Category.json");
+app.get("/category", (req, res) => {
+  res.send(category);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(category.find((c) => c.id === id));
+});
+const courses = require("./Data/courses.json");
+app.get("/course", (req, res) => {
+  res.send(courses);
+});
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const coursesById = courses.find((c) => c._id === id);
+  console.log(coursesById);
+  const coursesByCategoryId = courses.find((c) => c.category_id === id);
+  console.log(coursesByCategoryId);
+  res.send(coursesById || coursesByCategoryId);
+});
+
+app.listen(Port, () => {
+  console.log("server is running on: ", Port);
+});
